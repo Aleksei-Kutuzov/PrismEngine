51d8adda2e30bb935e79f34da9edac7733114dcd
Aleksei-Kutuzov <alekseikutuzov2@yandex.com>
2026-07-17 15:28:47 +0300

    change:  default value of ambientIntensity for CameraComponent

--- PrismEngine/src/cameraComponent.h
+++ PrismEngine/src/cameraComponent.h
@@ -46,7 +46,7 @@ namespace prism {
             bool useCurrentWindowAspect = true; /// Автоматически использовать соотношение сторон окна. Если true, параметр aspect игнорируется и берется соотношение сторон текущего окна/viewport'а
 
             glm::vec3 ambientColor = {0.3, 0.3, 0.2};
-            float ambientIntensity = 0.54;
+            float ambientIntensity = 0.02;
         };
     }
 }
